e38ee3791999d747779ca46ded0104e2e3534d20
Andreas Szekely <szandreas97@gmail.com>
2022-10-19 10:38:02 +0200

    Updating template (#1)
    * Updating dependencies and fixing next.config.js
    
    * Adding _document to pages
    
    * Adding example basic component
    
    * Added basic OpenGraph properties

---
+++ components/HelloWorld/HelloWorld.tsx
@@ -0,0 +1,11 @@
+import styles from './HelloWorld.module.scss';
+
+const HelloWorld = () => {
+  return (
+    <h1 className={styles.title}>
+        Hello world!
+    </h1>
+  );
+};
+
+export default HelloWorld;
